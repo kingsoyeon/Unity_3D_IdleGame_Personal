d6ab062f34c48c7956a7d1ae38a053800ad77c36
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-03-25 17:49:24 +0900

    feat : Health.cs, 체력 및 피격 메서드
    TakeDamage 함수 및 OnDie 액션

---
+++ 3D_IDLE_RPG_Personal/Assets/02.Scripts/Player/Health.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+
+    private int health;
+
+    public event Action OnDie; // 죽었을 때 호출될 액션
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (health == 0) return;
+
+        health = Mathf.Max(health-damage, 0);
+
+        // 데미지를 입고 피가 0이 되면 액션 호출
+        if (health == 0) OnDie?.Invoke();
+    }
+}
