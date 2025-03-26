0341723a3d177dfa81e925a7bcb400a350d9800e
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-03-26 21:33:59 +0900

    issue : hpBar.cs - health.cs 연결 문제
    오브젝트 풀링 방식을 사용하기 때문에 프리팹 내의 HPbar가 health를 참조하기 어려움, 구조 변경 필요함

--- 3D_IDLE_RPG_Personal/Assets/02.Scripts/Player/Health.cs
+++ 3D_IDLE_RPG_Personal/Assets/02.Scripts/Player/Health.cs
@@ -3,20 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Health : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 100;
 
     private int health;
 
+    public event Action<int, int> OnHealthChange; // 체력 변화할 때 호출될 액션
     public event Action OnDie; // 죽었을 때 호출될 액션
 
     public bool IsDie = false;
 
+    
+
     void Start()
     {
         InitialHealth();
-
+    
     }
 
     public void TakeDamage(int damage)
@@ -25,6 +29,9 @@ public class Health : MonoBehaviour
 
         health = Mathf.Max(health-damage, 0);
 
+        // 체력 변화 액션 호출
+        OnHealthChange?.Invoke(health, maxHealth);
+
         // 데미지를 입고 피가 0이 되면 액션 호출
         if (health == 0) // 죽음
         {
@@ -32,7 +39,6 @@ public class Health : MonoBehaviour
             
             OnDie?.Invoke();
         }
-        Debug.Log(health);
     }
 
     // 체력, isDie 초기화 메서드
---
+++ 3D_IDLE_RPG_Personal/Assets/02.Scripts/UI/HPbar.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPbar : MonoBehaviour, IPoolable
+{
+    public Health Health;
+    [SerializeField] private Image hpBar;
+
+    public void Initialize(Action<GameObject> returnAction)
+    {
+
+    }
+
+    public void OnDespawn()
+    {
+        Health.OnHealthChange -= UpdateHPBar;
+    }
+
+    public void OnSpawn()
+    {
+        Health.OnHealthChange += UpdateHPBar;
+    }
+
+    void UpdateHPBar(int health, int maxHealth)
+    {
+        hpBar.fillAmount = (float)health / maxHealth;
+    }
+}
