be15d47173142a6371ba95b7ce318b30927de693
KimSoyeon <107872180+kingsoyeon@users.noreply.github.com>
2025-03-26 22:02:48 +0900

    fix : hp바 주석처리

--- 3D_IDLE_RPG_Personal/Assets/02.Scripts/UI/HPbar.cs
+++ 3D_IDLE_RPG_Personal/Assets/02.Scripts/UI/HPbar.cs
@@ -4,28 +4,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HPbar : MonoBehaviour, IPoolable
+public class HPbar : MonoBehaviour//IPoolable
 {
-    public Health Health;
-    [SerializeField] private Image hpBar;
+    //public Health Health;
+    //[SerializeField] private Image hpBar;
 
     public void Initialize(Action<GameObject> returnAction)
     {
 
     }
 
-    public void OnDespawn()
-    {
-        Health.OnHealthChange -= UpdateHPBar;
-    }
+    //public void OnDespawn()
+    //{
+    //    Health.OnHealthChange -= UpdateHPBar;
+    //}
 
-    public void OnSpawn()
-    {
-        Health.OnHealthChange += UpdateHPBar;
-    }
+    //public void OnSpawn()
+    //{
+    //    Health.OnHealthChange += UpdateHPBar;
+    //}
 
-    void UpdateHPBar(int health, int maxHealth)
-    {
-        hpBar.fillAmount = (float)health / maxHealth;
-    }
+    //void UpdateHPBar(int health, int maxHealth)
+    //{
+    //    hpBar.fillAmount = (float)health / maxHealth;
+    //}
 }
